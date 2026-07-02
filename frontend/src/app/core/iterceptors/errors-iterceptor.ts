ae357bd4ced0eb545b729fe334881294ee753f7a
Lucas Moreno <lmorenooitabem@gmail.com>
2026-07-02 03:00:17 -0300

    fix: redirect to login when401

--- frontend/src/app/core/iterceptors/errors-iterceptor.ts
+++ frontend/src/app/core/iterceptors/errors-iterceptor.ts
@@ -1,14 +1,18 @@
+import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private readonly router: Router) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
           localStorage.removeItem('token');
+          this.router.navigate(['/login']);
         }
 
         if (error.status === 403) {
